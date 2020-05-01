6dc7aa9b51d49c042ac6dbd30f11220b9d52421a
unknown <rishavmittal2903@gmail.com>
2020-05-02 00:25:14 +0530

    Remove unnecessary file

--- app/Server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const paymentGatewayRouter=require('./Routes/PaymentGateway')
-var port =process.env.PORT||8080;
-app.use(express.static(__dirname));
-app.use(express.json())
-app.use('/paymentGateway',paymentGatewayRouter)
-app.listen(port, () => console.log('server started'))
